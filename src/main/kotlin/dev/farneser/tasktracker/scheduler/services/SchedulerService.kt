2e2444d4f24bb22bbda2711e4ac93abfe3db80b4
farneser <farneser1@gmail.com>
2023-11-26 21:11:04 +0300

    feat: scheduler service implementation

--- src/main/kotlin/dev/farneser/tasktracker/scheduler/services/SchedulerService.kt
+++ src/main/kotlin/dev/farneser/tasktracker/scheduler/services/SchedulerService.kt
@@ -1,6 +1,10 @@
 package dev.farneser.tasktracker.scheduler.services
 
+import dev.farneser.tasktracker.scheduler.dto.ColumnDto
+import dev.farneser.tasktracker.scheduler.dto.StatisticDto
 import dev.farneser.tasktracker.scheduler.services.messages.MessageService
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -22,25 +26,37 @@ class SchedulerService(
     @Scheduled(cron = "\${scheduler.cron:0 0 0 * * *}")
     fun scheduledNotifier() {
 
-        log.info("Scheduled task executed at ${System.currentTimeMillis()}")
+        log.info("Scheduled task started at ${System.currentTimeMillis()}")
 
         val users = userService.getUnsubscribedUsers()
 
         for (user in users) {
+
+            log.debug("User: {}", user)
+
+            val statistic = StatisticDto(user.email)
 
             val columns = columnService.getByUserId(user.id)
 
             for (column in columns) {
                 val tasks = taskService.getByColumnId(column.id, user.id)
 
-                log.info("Tasks: $tasks")
+                statistic.columns.add(ColumnDto(column, tasks))
+
+                log.debug("Tasks in column ${column.columnName}: $tasks")
             }
 
             val archivedTasks = taskService.getByColumnId(-1, user.id)
 
-            log.info("Archived tasks: $archivedTasks")
+            statistic.columns.add(ColumnDto("Archived", -1, archivedTasks))
 
-            messageService.sendScheduledMessage("")
+            log.debug("Archived tasks: $archivedTasks")
+
+            messageService.sendScheduledMessage(Json.encodeToString(statistic))
+
+            log.debug("User {} notified successfully with statistic: {}", user.email, statistic)
         }
+
+        log.info("Scheduled task finished at ${System.currentTimeMillis()}")
     }
 }
